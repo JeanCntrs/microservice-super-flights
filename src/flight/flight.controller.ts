224e46ad1fbfd5aedd46c1dbd7a7fed64f8f0a52
Jean C. Contreras <jean.cntrs@gmail.com>
2021-11-17 22:34:08 -0300

    Was added flight controller

--- src/flight/flight.controller.ts
+++ src/flight/flight.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { FlightMSG } from 'src/common/constants';
+import { IFlight } from 'src/common/interfaces/flight.interface';
+import { ClientProxySuperFlights } from 'src/common/proxy/client.proxy';
+import { FlightDTO } from './dto/flight.dto';
 
 @Controller('api/v2/flights')
-export class FlightController {}
+export class FlightController {
+    constructor(private readonly clientProxy: ClientProxySuperFlights) { }
+    private _clientProxyFlight = this.clientProxy.clientProxyFlights();
+
+    @Post()
+    create(@Body() flightDTO: FlightDTO): Observable<IFlight> {
+        return this._clientProxyFlight.send(FlightMSG.CREATE, flightDTO);
+    }
+
+    @Get()
+    findAll(): Observable<IFlight[]> {
+        return this._clientProxyFlight.send(FlightMSG.FIND_ALL, '');
+    }
+}
