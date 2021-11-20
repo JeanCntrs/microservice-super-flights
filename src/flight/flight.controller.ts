60c464825dfc3493ba8dbf035d51bc54543d6063
Jean C. Contreras <jean.cntrs@gmail.com>
2021-11-20 10:14:47 -0300

    Was added flight controller part 4

--- src/flight/flight.controller.ts
+++ src/flight/flight.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, Put } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { FlightMSG } from 'src/common/constants';
+import { FlightMSG, PassengerMSG } from 'src/common/constants';
 import { IFlight } from 'src/common/interfaces/flight.interface';
 import { ClientProxySuperFlights } from 'src/common/proxy/client.proxy';
 import { FlightDTO } from './dto/flight.dto';
@@ -9,6 +9,7 @@ import { FlightDTO } from './dto/flight.dto';
 export class FlightController {
     constructor(private readonly clientProxy: ClientProxySuperFlights) { }
     private _clientProxyFlight = this.clientProxy.clientProxyFlights();
+    private _clientProxyPassenger = this.clientProxy.clientProxyPassengers();
 
     @Post()
     create(@Body() flightDTO: FlightDTO): Observable<IFlight> {
@@ -21,12 +22,31 @@ export class FlightController {
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string, @Body() flightDTO: FlightDTO): Observable<IFlight> {
+    findOne(@Param('id') id: string): Observable<IFlight> {
         return this._clientProxyFlight.send(FlightMSG.FIND_ONE, id);
     }
 
     @Put(':id')
     update(@Param('id') id: string, @Body() flightDTO: FlightDTO): Observable<IFlight> {
-            return this._clientProxyFlight.send(FlightMSG.UPDATE, id);
+        return this._clientProxyFlight.send(FlightMSG.UPDATE, { id, flightDTO });
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string): Observable<any> {
+        return this._clientProxyFlight.send(FlightMSG.DELETE, id);
+    }
+
+    @Post(':flightId/passenger/:passengerId')
+    async addPassenger(
+        @Param('flightId') flightId: string,
+        @Param('passengerId') passengerId: string
+    ) {
+        const passenger = await this._clientProxyPassenger.send(PassengerMSG.FIND_ONE, passengerId).toPromise();
+
+        if (!passenger) {
+            throw new HttpException('Passenger not found', HttpStatus.NOT_FOUND);
+        }
+
+        return this._clientProxyFlight.send(FlightMSG.ADD_PASSENGER, { flightId, passengerId });
     }
 }
